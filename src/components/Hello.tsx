9003aaf977d873fbd050cf943a95bef1ebef7353
Maxime Dupont <emixamdupont@gmail.com>
2018-09-13 22:05:28 +0200

    Ajout dépendance Redux

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,48 +3,32 @@ import * as React from "react";
 import './Hello.css';
 
 export interface Props {
-  name: string;
-  enthusiasmLevel?: number;
+    name: string;
+    enthusiasmLevel?: number;
+    onIncrement?: () => void;
+    onDecrement?: () => void;
 }
 
-interface State {
-  currentEnthusiasm: number;
-}
-
-class Hello extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
-  }
-
-  onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
-  onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
-
-  render() {
-    const { name } = this.props;
-
-    if (this.state.currentEnthusiasm <= 0) {
-      throw new Error('You could be a little more enthusiastic. :D');
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+    if (enthusiasmLevel <= 0) {
+        throw new Error('You could be a little more enthusiastic. :D');
     }
-
+  
     return (
-      <div className="hello">
-        <div className="greeting">
-          Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
+        <div className="hello">
+            <div className="greeting">
+                Hello {name + getExclamationMarks(enthusiasmLevel)}
+            </div>
+            <div>
+                <button onClick={onDecrement}>-</button>
+                <button onClick={onIncrement}>+</button>
+            </div>
         </div>
-        <button onClick={this.onDecrement}>-</button>
-        <button onClick={this.onIncrement}>+</button>
-      </div>
     );
-  }
-
-  updateEnthusiasm(currentEnthusiasm: number) {
-    this.setState({ currentEnthusiasm });
-  }
 }
 
 export default Hello;
 
 function getExclamationMarks(numChars: number) {
-  return Array(numChars + 1).join('!');
+    return Array(numChars + 1).join('!');
 }
