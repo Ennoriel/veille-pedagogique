514dec519fe9b879292a213d44dcd6510c59e28e
Maxime Dupont <emixamdupont@gmail.com>
2018-09-13 13:34:06 +0200

    Ajout d'un exemple

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import Hello from './Hello';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Hello name="TypeScript" enthusiasmLevel={1} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
