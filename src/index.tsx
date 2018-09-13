0f65b3609c7033e3773bfb92c37f23fbcb747006
Maxime Dupont <emixamdupont@gmail.com>
2018-09-13 22:05:28 +0200

    Ajout dépendance Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
-import Hello from './components/Hello';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+
+const store = createStore<StoreState, any, any, any>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-    <Hello name="TypeScript" enthusiasmLevel={10} />,
+    <Provider store={store}>
+        <Hello />
+    </Provider>,
     document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
