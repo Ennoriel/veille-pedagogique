901c28ce471dbe0d85e2b75f77dfeae40729dfb0
Maxime Dupont <emixamdupont@gmail.com>
2018-09-14 22:24:46 +0200

    ajout d'un exemple d'appel HTTP avec Axios au chargement d'une page (composant passé en Statefull)

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,30 +1,53 @@
 import * as React from "react";
+import axios from 'axios';
 
 import './Hello.css';
 
 export interface Props {
     name: string;
-    enthusiasmLevel?: number;
+    enthusiasmLevel: number;
     onIncrement?: () => void;
     onDecrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
-    if (enthusiasmLevel <= 0) {
-        throw new Error('You could be a little more enthusiastic. :D');
+interface State {
+    persons: Array<{name: string}>
+}
+
+// ({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props)
+class Hello extends React.Component<Props> {
+    readonly state: State = {
+        persons: []
+      }
+
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => {
+                const persons = res.data;
+                this.setState({ persons });
+            })
+      }
+
+    render() {
+        if (this.props.enthusiasmLevel <= 0) {
+            throw new Error('You could be a little more enthusiastic. :D');
+        }
+      
+        return (
+            <div className="hello">
+                <div className="greeting">
+                    Hello {this.props.name + getExclamationMarks(this.props.enthusiasmLevel)}
+                </div>
+                <div className="button-group">
+                    <button onClick={this.props.onDecrement}>-</button>
+                    <button onClick={this.props.onIncrement}>+</button>
+                </div>
+                <ul>
+                    { this.state.persons.map(person => <li>{person.name}</li>)}
+                </ul>
+            </div>
+        );
     }
-  
-    return (
-        <div className="hello">
-            <div className="greeting">
-                Hello {name + getExclamationMarks(enthusiasmLevel)}
-            </div>
-            <div>
-                <button onClick={onDecrement}>-</button>
-                <button onClick={onIncrement}>+</button>
-            </div>
-        </div>
-    );
 }
 
 export default Hello;
