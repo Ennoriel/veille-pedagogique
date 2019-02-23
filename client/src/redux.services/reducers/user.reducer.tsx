bccc06a5291c63fd8229bc3376f6c3273774251f
Maxime Dupont <emixamdupont@gmail.com>
2019-02-23 07:28:40 +0100

    Suppression doc

--- client/src/redux.services/reducers/user.reducer.tsx
+++ client/src/redux.services/reducers/user.reducer.tsx
@@ -8,17 +8,10 @@ const defaultUser: IUser = {
 export default (state: IUser = defaultUser, action: SaveUserData | Logout) => {
     switch(action.type) {
         case SAVE_USER_DATA:
-
-            // const userRight = action.payload.token === undefined ? UserRight.NOT_AUTH :
-            //     action.payload.user.right === undefined ? UserRight.NOT_AUTH : 
-            //     action.payload.user.right === "NOT_AUTHORIZED" ? UserRight.BEARER_0 :
-            //     action.payload.user.right === "AUTHORIZED" ? UserRight.BEARER_1 : UserRight.SUPER_USER;
-            console.log(action.payload.user)
             return {
                 ...state,
                 ...action.payload.user,
-                token: action.payload.token,
-                // userRight
+                token: action.payload.token
             };
         case LOGOUT:
             return defaultUser;
